a4464066311d7cd7ecd339858077bcbb9785fbb8
ankush <jamuar66@gmail.com>
2026-03-16 00:40:04 +0530

    fix express request typings

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,10 +1,10 @@
-import { JwtPayload } from 'jsonwebtoken';
+import { JwtPayload } from "jsonwebtoken";
 
 declare global {
   namespace Express {
     interface Request {
       user?: JwtPayload | any;
-      app: any;
+      workspaceId?: string;
     }
   }
 }
